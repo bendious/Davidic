8be027cf9f90db9a62bf460cd8f6bb0a8f641aff
bendious <benjamin.laws@comcast.net>
2021-10-25 12:06:20 -0500

    MusicPlayer{UI} separation
    - split MusicPlayer{.cs} into MusicPlayer{.cs} and MusicPlayerUI{.cs}
    	- made MusicPlayerUI a pass-through component handling the UI-specific logic
    	- MusicPlayerUI.m_player to handle the internal music logic
    - moved scale/chord/rhythm randomization from MusicSequencer to MusicPlayer
    	- removed MusicSequencer.m_{rootKey/scale}Index and regen* params from MusicSequencer()
    
    - fixed some inconsistent left-hand spacing in scene

---
+++ Assets/Scripts/Components/MusicPlayerUI.cs
@@ -0,0 +1,111 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayerUI : MonoBehaviour
+{
+	public uint m_samplesPerSecond = 44100U;
+	public bool m_stereo = true;
+	public uint m_maxPolyphony = 40U;
+
+	public InputField m_tempoField;
+	public Toggle m_scaleRegenToggle;
+	public Dropdown m_rootNoteDropdown;
+	public Dropdown m_scaleDropdown;
+	public ScrollRect m_instrumentScrollview;
+	public Toggle m_chordRegenToggle;
+	public InputField m_chordField;
+	public Toggle m_rhythmRegenToggle;
+	public InputField m_rhythmField;
+	public InputField[] m_noteLengthFields;
+	public InputField m_harmonyCountField;
+	public InputField m_instrumentCountField;
+	public InputField m_volumeField;
+
+	public string m_bankFilePath = "GM Bank/gm";
+
+
+	private MusicPlayer m_player;
+
+
+	public void Start()
+	{
+		m_player = new MusicPlayer();
+		m_player.Start();
+
+		// enumerate instruments in UI
+		string[] instrumentNames = m_player.InstrumentNames();
+		Assert.AreEqual(m_instrumentScrollview.content.childCount, 1);
+		GameObject placeholderObj = m_instrumentScrollview.content.GetChild(0).gameObject;
+		Assert.IsFalse(placeholderObj.activeSelf);
+		Vector2 nextPos = placeholderObj.GetComponent<RectTransform>().anchoredPosition;
+		float startY = nextPos.y;
+		float lineHeight = Mathf.Abs(placeholderObj.GetComponent<RectTransform>().rect.y);
+		for (int i = 0, n = instrumentNames.Length; i < n; ++i)
+		{
+			if (instrumentNames[i] == null)
+			{
+				continue;
+			}
+			string instrumentName = instrumentNames[i];
+
+			GameObject toggleObj = Instantiate(placeholderObj, m_instrumentScrollview.content.transform);
+			toggleObj.name = instrumentName;
+			toggleObj.GetComponentInChildren<Text>().text = instrumentName;
+			toggleObj.GetComponent<RectTransform>().anchoredPosition = nextPos;
+			toggleObj.SetActive(true);
+
+			nextPos.y -= lineHeight;
+		}
+		m_instrumentScrollview.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Abs(nextPos.y + startY));
+	}
+
+	public void Generate(bool isScale)
+	{
+		// parse input
+		float[][] chordList = m_chordField.text.Length == 0 ? new float[][] { } : m_chordField.text.Split(new char[] { ';' }).Select(str => str.Split(new char[] { ',' }).Select(str => float.Parse(str)).ToArray()).ToArray();
+		uint[] rhythmLengths = m_rhythmField.text.Length == 0 ? new uint[] { } : m_rhythmField.text.Split(new char[] { ';' }).Select(str => uint.Parse(str.Split(new char[] { ',' })[0])).ToArray();
+		float[] rhythmChords = m_rhythmField.text.Length == 0 ? new float[] { } : m_rhythmField.text.Split(new char[] { ';' }).Select(str => float.Parse(str.Split(new char[] { ',' })[1])).ToArray();
+
+		// pass input through
+		m_player.m_samplesPerSecond = m_samplesPerSecond;
+		m_player.m_stereo = m_stereo;
+		m_player.m_maxPolyphony = m_maxPolyphony;
+		m_player.m_tempo = uint.Parse(m_tempoField.text);
+		m_player.m_scaleRegen = m_scaleRegenToggle.isOn;
+		m_player.m_rootNoteIndex = (uint)m_rootNoteDropdown.value;
+		m_player.m_scaleIndex = (uint)m_scaleDropdown.value;
+		m_player.m_instrumentToggles = m_instrumentScrollview.content.GetComponentsInChildren<Toggle>().Select(toggle => toggle.isOn).ToArray();
+		m_player.m_chordRegen = m_chordRegenToggle.isOn;
+		m_player.m_chords = new ChordProgression(chordList);
+		m_player.m_rhythmRegen = m_rhythmRegenToggle.isOn;
+		m_player.m_rhythm = new MusicRhythm(rhythmLengths, rhythmChords);
+		m_player.m_noteLengthWeights = m_noteLengthFields.Select(field => float.Parse(field.text)).ToArray();
+		m_player.m_harmonyCount = uint.Parse(m_harmonyCountField.text);
+		m_player.m_instrumentCount = uint.Parse(m_instrumentCountField.text);
+		m_player.m_volume = float.Parse(m_volumeField.text);
+		m_player.m_bankFilePath = m_bankFilePath;
+
+		// generate
+		m_player.Generate(isScale);
+
+		// update display
+		m_rootNoteDropdown.value = (int)m_player.m_rootNoteIndex;
+		m_rootNoteDropdown.RefreshShownValue();
+		m_scaleDropdown.value = (int)m_player.m_scaleIndex;
+		m_scaleDropdown.RefreshShownValue();
+		m_chordField.text = m_player.m_chords.m_progression.Aggregate("", (str, chord) => str + (str == "" ? "" : ";") + chord.Aggregate("", (str, idx) => str + (str == "" ? "" : ",") + idx));
+		m_rhythmField.text = m_player.m_rhythm.m_lengthsSixtyFourths.Zip(m_player.m_rhythm.m_chordIndices, (a, b) => a + "," + b).Aggregate((a, b) => a + ";" + b);
+		MusicDisplay.Start();
+		m_player.Display("osmd-chords", "osmd-rhythm", "osmd-main");
+		MusicDisplay.Finish();
+	}
+
+	public void Play()
+	{
+		m_player.Play(GetComponent<AudioSource>());
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,118 @@
+using CSharpSynth.Synthesis;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class MusicPlayer
+{
+	public uint m_samplesPerSecond = 44100U;
+	public bool m_stereo = true;
+	public uint m_maxPolyphony = 40U;
+
+	public uint m_tempo;
+	public bool m_scaleRegen;
+	public uint m_rootNoteIndex;
+	public uint m_scaleIndex;
+	public bool[] m_instrumentToggles;
+	public bool m_chordRegen;
+	public ChordProgression m_chords;
+	public bool m_rhythmRegen;
+	public MusicRhythm m_rhythm;
+	public float[] m_noteLengthWeights;
+	public uint m_harmonyCount;
+	public uint m_instrumentCount;
+	public float m_volume;
+
+	public string m_bankFilePath = "GM Bank/gm";
+
+
+	private StreamSynthesizer m_musicStreamSynthesizer;
+	private MusicSequencer m_musicSequencer;
+	private uint[] m_instrumentIndices;
+
+
+	public void Start()
+	{
+		// create synthesizer
+		// TODO: recreate if relevant params change?
+		int channels = m_stereo ? 2 : 1;
+		const int bytesPerBuffer = 4096; // TODO?
+		m_musicStreamSynthesizer = new StreamSynthesizer((int)m_samplesPerSecond, channels, bytesPerBuffer / channels, (int)m_maxPolyphony);
+		m_musicStreamSynthesizer.LoadBank(m_bankFilePath);
+	}
+
+    public string[] InstrumentNames() => m_musicStreamSynthesizer.SoundBank.getInstruments(false/*?*/).Where(instrument => instrument != null).Select(instrument => instrument.Name).ToArray();
+
+    public void Generate(bool isScale)
+	{
+		// choose instrument
+		List<int> candidateIndices = new List<int>();
+		for (int i = 0, n = m_instrumentToggles.Length; i < n; ++i)
+		{
+			if (m_instrumentToggles[i])
+			{
+				candidateIndices.Add(i);
+			}
+		}
+		if (candidateIndices.Count <= 0)
+		{
+			return;
+		}
+		List<uint> instrumentList = new List<uint>();
+		for (uint i = 0U, n = m_instrumentCount; i < n; ++i)
+		{
+			instrumentList.Add((uint)candidateIndices[UnityEngine.Random.Range(0, candidateIndices.Count)]);
+		}
+		m_instrumentIndices = instrumentList.Distinct().ToArray();
+
+		// regen any random elements
+		if (m_scaleRegen)
+		{
+			m_rootNoteIndex = (uint)UnityEngine.Random.Range(0, 8);
+			m_scaleIndex = (uint)UnityEngine.Random.Range(0, MusicUtility.scales.Length);
+		}
+		if (m_chordRegen || m_chords.m_progression.Length <= 0)
+		{
+			m_chords = isScale ? new ChordProgression(new float[][] { MusicUtility.chordI, MusicUtility.chordII, MusicUtility.chordIII, MusicUtility.chordIV, MusicUtility.chordV, MusicUtility.chordVI, MusicUtility.chordVII, MusicUtility.chordI.Select(index => index + MusicUtility.tonesPerOctave).ToArray() }) : MusicUtility.chordProgressions[UnityEngine.Random.Range(0, MusicUtility.chordProgressions.Length)];
+		}
+		if (m_rhythmRegen || m_rhythm.m_lengthsSixtyFourths.Length <= 0)
+		{
+			m_rhythm = isScale ? new MusicRhythm(new uint[] { MusicUtility.sixtyFourthsPerBeat / 2U }, new float[] { 0.0f }) : MusicRhythm.Random(m_chords, m_noteLengthWeights);
+		}
+
+		// create sequencer
+		m_musicSequencer = new MusicSequencer(m_musicStreamSynthesizer, isScale, (uint)m_rootNoteIndex, (uint)m_scaleIndex, m_instrumentIndices, m_tempo, m_noteLengthWeights, m_harmonyCount, m_chords, m_rhythm);
+	}
+
+	public void Display(string elementIdChords, string elementIdRhythm, string elementIdMain)
+	{
+		if (m_musicSequencer != null)
+		{
+			string[] instrumentNames = m_instrumentIndices.Select(index => m_musicStreamSynthesizer.SoundBank.getInstrument((int)index, false/*?*/).Name).ToArray();
+			m_musicSequencer.Display(elementIdChords, elementIdRhythm, elementIdMain, instrumentNames, m_tempo);
+		}
+	}
+
+	public void Play(AudioSource source)
+	{
+		Assert.IsNotNull(source);
+		if (m_musicSequencer == null)
+		{
+			return;
+		}
+		source.volume = m_volume;
+		source.clip = AudioClip.Create("Generated Clip", (int)m_musicSequencer.LengthSamples, m_stereo ? 2 : 1, (int)m_samplesPerSecond, false, OnAudioRead, OnAudioSetPosition);
+		source.Play();
+	}
+
+
+	private void OnAudioRead(float[] data)
+	{
+		m_musicStreamSynthesizer.GetNext(data);
+		// NOTE that we don't increment m_musicSequencer since m_musicStreamSynthesizer takes care of that
+	}
+
+	private void OnAudioSetPosition(int new_position) => m_musicSequencer.SetTime(new System.TimeSpan(new_position));
+}
